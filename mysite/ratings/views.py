7cdddc4fc43baab5db81180ba37357a326322d0f
Jessica Langston <jessica.langston@rd.io>
2015-12-08 16:58:23 -0800

    Add handling of 404 errors for users and movies that don't exist

--- mysite/ratings/views.py
+++ mysite/ratings/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.template import RequestContext, loader
+from django.shortcuts import get_object_or_404, render
 
 from .models import User, Movie, Rating
 
@@ -8,16 +7,19 @@ from .models import User, Movie, Rating
 def index(request):
 	return render(request, 'ratings/index.html')
 
+
 def user_list(request):
 	""" Displays a list of users """
 	user_list = User.objects.order_by('id')
 	context = {'user_list': user_list}
 	return render(request, 'ratings/user_list.html', context)
 
+
 def user_details(request, user_id):
 	""" Displays information about a given user """
-	response = "You're viewing details about user %s"
-	return HttpResponse(response % user_id)
+	
+	user = get_object_or_404(User, pk=user_id)
+	return render(request, 'ratings/user_details.html', {'user': user})
 
 def movie_list(request):
 	""" Displays a list of movies """
@@ -25,7 +27,9 @@ def movie_list(request):
 	context = {'movie_list': movie_list}
 	return render(request, 'ratings/movie_list.html', context)
 
+
 def movie_details(request, movie_id):
 	""" Displays information about a given movie """
-	response = "You're viewing details about the movie %s"
-	return HttpResponse(response % movie_id)
+
+	movie = get_object_or_404(Movie, pk=movie_id)
+	return render(request, 'ratings/movie_details.html', {'movie': movie})
